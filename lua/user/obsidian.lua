948cc19ca6536f8d725e698751174ae5a0fafabe
dgiacomo <dgiacomo@mux.com>
2023-12-31 08:24:37 -0800

    fix obsidian setup file

--- lua/user/obsidian.lua
+++ lua/user/obsidian.lua
@@ -1,6 +1,9 @@
-config = function()
-  require("obsidian").setup({
-    use_advanced_uri = true,
-    dir = "~/vimwiki/KnowledgeBase"
-  })
+local status_ok, obsidian = pcall(require, "obsidian")
+  if not status_ok then
+    return
 end
+
+obsidian.setup{
+  use_advanced_uri = true,
+  dir = "~/vimwiki/KnowledgeBase"
+}
