68bff1df3e2f3e56a197648fead7258e580cbd17
dgiacomo <dgiacomo@mux.com>
2024-01-01 10:20:59 -0800

    setup obsidian Diary template and templates folder

--- lua/user/obsidian.lua
+++ lua/user/obsidian.lua
@@ -5,5 +5,22 @@ end
 
 obsidian.setup{
   use_advanced_uri = true,
-  dir = "~/vimwiki/KnowledgeBase"
+  dir = "~/vimwiki/",
+  templates = {
+    subdir = "templates",
+    date_format = "%Y-%m-%d",
+    time_format = "%H:%M",
+    -- A map for custom variables, the key should be the variable and the value a function
+    substitutions = {},
+  },
+  daily_notes = {
+    -- Optional, if you keep daily notes in a separate directory.
+    folder = "diary",
+    -- Optional, if you want to change the date format for the ID of daily notes.
+    -- date_format = "%Y-%m-%d",
+    -- Optional, if you want to change the date format of the default alias of daily notes.
+    alias_format = "%B %-d, %Y",
+    -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+    template = "Diary.md"
+  },
 }
