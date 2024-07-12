e90a6d774978e995a5a8e4d003cafe0f14414a60
dgiacomo <dgiacomo@mux.com>
2024-07-12 09:43:43 -0700

    woops, different name for lua

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -36,7 +36,7 @@ local options = {
   sidescrolloff = 8,                       -- minimal number of screen columns either side of cursor if wrap is `false`
   guifont = "monospace:h17",               -- the font used in graphical neovim applications
   whichwrap = "bs<>[]hl",                  -- which "horizontal" keys are allowed to travel to prev/next line
-  nofoldenable = true,                     -- don't fold code by default
+  foldenable = false,                     -- don't fold code by default
 }
 
 for k, v in pairs(options) do
